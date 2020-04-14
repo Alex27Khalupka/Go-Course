80b02262b7db2090454519c8d0e0852ad7b7ffef
Alex Khalupka <alexkha2001@gmail.com>
2020-04-14 17:51:08 +0300

    Added comments, deleted reduant 'else'

--- FirstHomework/Task1.go
+++ FirstHomework/Task1.go
@@ -6,22 +6,19 @@ import (
 )
 
 func main() {
-	array := []int{9, 7, 2, 5, 3, 6}
-	fmt.Println(median(array))
+	EvenArray := []int{9, 7, 2, 5, 3, 6}
+	OddArray := []int{2, 5, 11, 6, 8, 9, 10}
+	fmt.Println(median(EvenArray))
+	fmt.Println(median(OddArray))
 }
 
-func median(i []int) float64{
+// Returns the median of the array
+func median(i []int) float64 {
 	sort.Ints(i)
-	var med float64
 
-	if len(i) % 2 == 1{
-		med = float64(i[len(i) / 2])
-		return med
-
-	} else {
-		med = float64(i[len(i) / 2] + i[len(i) / 2 - 1]) / 2
-		return med
+	if len(i)%2 == 1 {
+		return float64(i[len(i)/2])
 	}
+
+	return float64(i[len(i)/2]+i[len(i)/2-1]) / 2
 }
-
-
--- FirstHomework/Task2.go
+++ FirstHomework/Task2.go
@@ -2,28 +2,37 @@ package main
 
 import "fmt"
 
+// Represents a point with (x, y) coordinates
 type Point struct {
 	x, y int
 }
 
+// Represents a square with the coordinate
+// of the upper left point(variable start) and side length
+// equal to variable a
 type Square struct {
 	start Point
-	a uint
+	a     uint
 }
 
-func (s Square) End () Point {
+// Returns the coordinates of the
+// bottom left point of a square
+func (s Square) End() Point {
 	var end Point
 	end.x = s.start.x + int(s.a)
 	end.y = s.start.y + int(s.a)
 	return end
 }
 
-func (s Square) Perimeter () uint {
-	return s.a*4
+// Returns the perimeter of the square
+func (s Square) Perimeter() uint {
+	return s.a * 4
+
 }
 
-func (s Square) Area () uint {
-	return s.a*s.a
+// Returns the area of the square
+func (s Square) Area() uint {
+	return s.a * s.a
 }
 
 func main() {
